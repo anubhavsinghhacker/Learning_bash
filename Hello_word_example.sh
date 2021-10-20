e60467a51c3352924e94d3799cbad98e5e6f3085
Darshan Jogi <64300256+darshanjogi@users.noreply.github.com>
2021-10-20 10:44:15 +0530

    created Hello_word_example.sh
    Hello_word_example.sh

---
+++ Hello_word_example.sh
@@ -0,0 +1,3 @@
+###You can run bash script from the terminal or by executing any bash file. Run the following command from the terminal to execute a very simple bash statement. The output of the command will be ‘Hello World’. 
+
+$ echo "Hello World" 
