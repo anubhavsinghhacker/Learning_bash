d3f3267fd38e49f3d56c44784fb5e0fce116fb18
Pramod Sargar <41449799+impramodsargar@users.noreply.github.com>
2021-10-21 22:33:48 +0530

    Update Hello_word_example.sh

--- Hello_word_example.sh
+++ Hello_word_example.sh
@@ -1,3 +1,61 @@
 ###You can run bash script from the terminal or by executing any bash file. Run the following command from the terminal to execute a very simple bash statement. The output of the command will be ‘Hello World’. 
 
 $ echo "Hello World" 
+#include "Complex.h"
+
+Complex::Complex()
+
+{
+
+    this->img=0;
+
+    this->real=0;
+
+}
+
+Complex::Complex(int real,int img)
+
+{
+
+    this->real=real;
+
+    this->img=img;
+
+}
+
+Complex Complex::operator+(Complex c)
+
+{
+
+    Complex com(this->real+c.real,this->img+c.img);
+
+    return com;
+
+}
+
+Complex Complex::operator-(Complex c)
+
+{
+
+    Complex com(this->real-c.real,this->img-c.img);
+
+    return com;
+
+}
+
+int Complex::getImg()
+
+{
+
+    return this->img;
+
+}
+
+int Complex::getReal()
+
+{
+
+    return this->real;
+
+}
+
